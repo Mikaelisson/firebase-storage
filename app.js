4d0e407ee0275fc242ac3028558ee3bd70879d1b
Mikaelisson <mikaelissongesuino@gmail.com>
2022-10-22 00:03:35 -0300

    Config CORS

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const path = require("path");
 const router = require("./routes/router");
 
 app.use(express.static(path.join(__dirname, "front/build")));
 
-const corsOptions = {
-  origin: "https://firebase-storage-test.onrender.com",
-  optionsSuccessStatus: 200,
-};
-
 if (process.env.NODE_ENV != "development") {
-  app.options("*", cors());
-
   app.use(express.static(path.join(__dirname, "front/build")));
 
   app.get("*", (req, res) => {
--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const cors = require("cors");
+
 const db = require("../controllers/db");
 
 const multer = require("multer");
@@ -9,8 +11,14 @@ const firebaseStorage = require("../firebase/firebaseConfig.js");
 
 const controller = require("../controllers/controller");
 
+const corsOptions = {
+  origin: "http://firebase-storage-test.onrender.com",
+  optionsSuccessStatus: 200,
+};
+
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
+router.use(cors(corsOptions));
 
 router.get("/", controller.home);
 router.get("/api/images", db.findDocuments, controller.findImage);
