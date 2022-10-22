576b99f39b34d7acddfe3e6e485fafae91cd6d5d
Mikaelisson <mikaelissongesuino@gmail.com>
2022-10-22 00:22:18 -0300

    Remove CORS

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const cors = require("cors");
-
 const db = require("../controllers/db");
 
 const multer = require("multer");
@@ -11,14 +9,8 @@ const firebaseStorage = require("../firebase/firebaseConfig.js");
 
 const controller = require("../controllers/controller");
 
-const corsOptions = {
-  origin: "https://firebase-storage-test.onrender.com",
-  optionsSuccessStatus: 200,
-};
-
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
-router.use(cors(corsOptions));
 
 router.get("/", controller.home);
 router.get("/api/images", db.findDocuments, controller.findImage);
